ba306ffa244147c8123c159bcb33aa8b73fbebe6
David de la Mora <david.dlm.lewagon@gmail.com>
2025-05-07 13:44:09 +0000

    int_campaigns_day.sql

---
+++ models/intermediate/int_campaigns_day.sql
@@ -0,0 +1,8 @@
+select
+    date_date
+    ,sum(ads_cost)
+    ,sum(impression) as ads_impressions
+    ,sum(click) as ads_clicks
+from {{ref("int_campaigns")}}
+group by date_date
+order by date_date desc
