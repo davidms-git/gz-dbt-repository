9a849ec0fa15c6f2a9a2ac3a65c4968afaebc180
David de la Mora <david.dlm.lewagon@gmail.com>
2025-05-07 08:33:35 +0000

    int_sales_margin

--- models/intermediate/int_sales_margin.sql
+++ models/intermediate/int_sales_margin.sql
@@ -1,24 +1,13 @@
---margin = revenue - purchase_cost
---purchase_cost = quantity * purchase_price
-
-WITH cost AS(
 SELECT
-   s.products_id
-   -- ,s.revenue
-    --,s.quantity
-    --,p.purchase_price
-    ,CAST(SUM(s.quantity*p.purchase_price) AS FLOAT64) AS purchase_cost
-FROM {{ref('stg_raw__sales')}} s
-JOIN {{ref('stg_raw__product')}} p
-ON s.products_id = p.products_id
-GROUP BY s.products_id
-)
-SELECT
-    s.products_id
-    ,purchase_cost
-    ,SUM(s.revenue-cost.purchase_cost) AS margin
-FROM {{ref('stg_raw__sales')}} s
-JOIN cost
-ON s.products_id = cost.products_id
-GROUP BY s.products_id, purchase_cost
+      products_id,
+      date_date,
+      orders_id,
+      revenue,
+      quantity,
+      purchase_price,
+      ROUND(s.quantity*p.purchase_price,2) AS purchase_cost,
+      ROUND(s.revenue - s.quantity*p.purchase_price, 2) AS margin
+  FROM {{ref("stg_raw__sales")}} s
+  LEFT JOIN {{ref("stg_raw__product")}} p
+      USING (products_id)
 
